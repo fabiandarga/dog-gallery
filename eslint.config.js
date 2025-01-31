99df3d577ef7b34c42806d6533bb75c9c8f1dc05
Fabian Darga <kontakt@fabiandarga.de>
2025-01-31 15:18:37 +0100

    fix depcheck

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactQuery from '@tanstack/eslint-plugin-query'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -16,6 +17,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react-query': reactQuery,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
